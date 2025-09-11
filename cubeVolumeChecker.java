ea99049481cb9e1310761f978cb009f198843f93
Shree Hari P <shreehariparthiban159@gmail.com>
2025-09-11 19:59:26 +0530

    Java Static Initializer Block

---
+++ cubeVolumeChecker.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class cubeVolumeChecker {
+    
+    static int a;
+    static boolean flag;
+
+    static {
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+
+        if(a > 0){
+            flag = true;
+        } else {
+            flag = false;
+            System.out.println("Error");
+        }
+    }
+
+    public static void main(String[] args){
+        if(flag){
+            double volume = Math.pow(a , 3);
+            System.out.println("Volume of Cube: "+volume);
+        }
+    }
+
+}
